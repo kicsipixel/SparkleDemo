6aa6833e2fccfe00121aea2756dd586ce8261dbc
Szabolcs Tóth <tsz@purzelbaum.hu>
2023-06-17 13:47:29 +0200

    New: Initial commit.

---
+++ SparkleDemo/SparkleDemoApp.swift
@@ -0,0 +1,19 @@
+//
+//  SparkleDemoApp.swift
+//  SparkleDemo
+//
+//  Created by Szabolcs Tóth on 17.06.2023.
+//  Copyright © 2023 Szabolcs Tóth. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SparkleDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .frame(width: 640, height: 480)
+        }
+    }
+}
